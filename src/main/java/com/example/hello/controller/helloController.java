e3073832fecc4af8d4c0717739588df29d852f1a
ashishra <ashishra@maveric-systems.com>
2022-06-15 12:19:11 +0530

    changes for service discovery and service communication

--- src/main/java/com/example/hello/controller/helloController.java
+++ src/main/java/com/example/hello/controller/helloController.java
@@ -3,13 +3,19 @@ package com.example.hello.controller;
 import com.example.hello.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/gateway")
 public class helloController {
 
     @Autowired
     private EmailSenderService emailSenderService;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/hello1")
     public String getHello()  {
@@ -20,7 +26,17 @@ public class helloController {
         }
     }
 
-    @GetMapping("/mail")
+    @GetMapping("/hello2")
+    public String hello2()  {
+        return restTemplate.getForObject("http://hello2/hello2", String.class);
+    }
+
+    @GetMapping("/hello3")
+    public String hello3()  {
+        return restTemplate.getForObject("http://hello3/hello3", String.class);
+    }
+
+    @GetMapping("/mail") // sends mail only when connected to personal network and not Maveric network as its accessing gmail
     public String triggerMail()  {
         emailSenderService.sendSimpleEmail("secularyoung8@gmail.com",
                 "hello hello", "testing");
